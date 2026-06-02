80ce350fef0180179a832474db08adadf8e54b26
4D-16\koyo <a>
2026-06-02 14:44:19 +0900

    コンストラクタテーブル

---
+++ Element.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// @brief —v‘f‚ğ•\‚·—ñ‹“Œ^
+enum Element
+{
+	/// <summary>
+	/// ‘®«‚È‚µ
+	/// </summary>
+	None,
+	/// <summary>
+	/// ‰Î‘®«
+	/// </summary>
+	Fire,
+	/// <summary>
+	/// …‘®«
+	/// </summary>
+	Water,
+	/// <summary>
+	/// “y‘®«
+	/// </summary>
+	Earth,
+	/// <summary>
+	/// •—‘®«
+	/// </summary>
+	Wind,
+	/// <summary>
+	/// Œõ‘®«
+	/// </summary>
+	Light,
+	/// <summary>
+	/// ˆÅ‘®«
+	/// </summary>
+	Dark
+};
---
+++ Enemy.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "EnemyData.h"
+
+class Enemy
+{
+public:
+
+    EnemyData Data;
+
+    Enemy(const EnemyData& data)
+        :Data(data)
+    {
+    }
+
+};
